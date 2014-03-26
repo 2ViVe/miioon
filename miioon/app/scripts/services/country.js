d77747caa63b07f61cd7ca727984d64983eb8205
Liu Yang <zation1@gmail.com>
2014-03-26 11:15:51 +0800

    only show countries with states

--- miioon/app/scripts/services/country.js
+++ miioon/app/scripts/services/country.js
@@ -5,7 +5,16 @@ angular.module('2ViVe')
     function($http) {
       return {
         list: function() {
-          return $http.get('/api/v2/countries');
+          return $http.get('/api/v2/countries')
+            .success(function(countries) {
+              var countriesWithStates = [];
+              angular.forEach(countries.response, function(country) {
+                if (country.states.length > 0) {
+                  countriesWithStates.push(country);
+                }
+              });
+              countries.response = countriesWithStates;
+            });
         }
       };
     }]);
