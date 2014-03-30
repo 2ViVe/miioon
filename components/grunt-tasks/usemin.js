7e55cb7d25854b028e964be69aa2ddc63268ea86
Liu Yang <zation1@gmail.com>
2014-03-30 09:18:42 +0800

    modify usemin to generate all images

--- components/grunt-tasks/usemin.js
+++ components/grunt-tasks/usemin.js
@@ -1,7 +1,7 @@
 module.exports = (function() {
   // Performs rewrites based on rev and the useminPrepare configuration
   return {
-    html: ['<%= yeoman.dist %>/{,*/}*.html'],
+    html: ['<%= yeoman.dist %>/{,**/}*.html'],
     css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
     options: {
       assetsDirs: ['<%= yeoman.dist %>']
