c168871422afa57b9905da627390844ad0905b7c
after9 <ap66.ron@gmail.com>
2014-04-09 21:30:00 +0800

    add images switching

--- miioon/app/scripts/gift/controller.js
+++ miioon/app/scripts/gift/controller.js
@@ -3,16 +3,27 @@
 angular.module('2ViVe')
   .controller('GiftController', ['$scope',
     function($scope) {
+      // images switching
+  
+      $scope.giftImagesBig='images/gift/gift-img-big-1.jpg';
+      $scope.giftImagesIndex = '1';
+      $scope.switchGiftImages = function(target){
+        $scope.giftImagesIndex = target.getAttribute('data');
+        $scope.giftImagesBig = 'images/gift/gift-img-big-' + $scope.giftImagesIndex + '.jpg';
+      }
+      
+      //tabs
       $scope.tabs = [{
         'title': 'EMAIL',
         //'content': 'email.html'
         url: "email.html"
-      }, {
-        'title': 'POST',
-        //'content': 'post.html'
-        url: 'post.html'
-      }];
-    }]);
+        }, {
+          'title': 'POST',
+          //'content': 'post.html'
+          url: 'post.html'
+        }];
+      }]
+    );
 
 
 
