1f6d5279d112244cf682f79f5cec37b1152fa506
Liu Yang <zation1@gmail.com>
2014-04-10 01:06:45 +0800

    update banner image for accessary

--- miioon/app/scripts/navigation/controller.js
+++ miioon/app/scripts/navigation/controller.js
@@ -3,7 +3,7 @@
 angular.module('2ViVe')
   .controller('MainNavigationController', ['$scope', 'Taxons',
     function($scope, Taxons) {
-      $scope.taxons = Taxons.getByPositionMoreThan(0);
+
       $scope.$watch(function() {
         return Taxons.data;
       }, function() {
--- miioon/app/scripts/taxon/controller.js
+++ miioon/app/scripts/taxon/controller.js
@@ -6,17 +6,20 @@ angular.module('2ViVe')
       var taxonId = Number($routeParams.taxonId);
       var subTaxonId = Number($routeParams.subTaxonId);
 
-      $scope.taxon = Taxons.getById(taxonId);
-      $scope.currentSubTaxon = Taxons.getSubTaxonById($scope.taxon, subTaxonId);
-
       $scope.$watch(function() {
         return Taxons.data;
       }, function() {
         $scope.taxon = Taxons.getById(taxonId);
         $scope.currentSubTaxon = Taxons.getSubTaxonById($scope.taxon, subTaxonId);
-      });
 
-      Product.getByTaxon(subTaxonId).success(function(data) {
-        $scope.products = data.response.products;
+        if ($scope.currentSubTaxon === null) {
+          Product.getByTaxon(taxonId).success(function(data) {
+            $scope.products = data.response.products;
+          });
+        } else {
+          Product.getByTaxon(subTaxonId).success(function(data) {
+            $scope.products = data.response.products;
+          });
+        }
       });
     }]);
