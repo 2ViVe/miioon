d589bc2fd5817d6412699ef2de442dab80d82178
Liu Yang <zation1@gmail.com>
2014-04-10 01:37:26 +0800

    diable image renaming

--- components/grunt-tasks/rev.js
+++ components/grunt-tasks/rev.js
@@ -6,7 +6,6 @@ module.exports = (function() {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
