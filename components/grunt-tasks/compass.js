51dcec6297ab2493c0352dfb953f5b9ac17fd9e2
Liu Yang <zation1@gmail.com>
2014-04-16 22:30:58 +0800

    update compass image setting

--- components/grunt-tasks/compass.js
+++ components/grunt-tasks/compass.js
@@ -5,7 +5,7 @@ module.exports = (function() {
       sassDir: '<%= yeoman.app %>/styles',
       cssDir: '.tmp/styles',
       generatedImagesDir: '.tmp/images/generated',
-      imagesDir: '<%= yeoman.app %>/styles/images',
+      imagesDir: '<%= yeoman.app %>/images',
       javascriptsDir: '<%= yeoman.app %>/scripts',
       fontsDir: '<%= yeoman.app %>/styles/fonts',
       importPath: '<%= yeoman.app %>/bower_components',
