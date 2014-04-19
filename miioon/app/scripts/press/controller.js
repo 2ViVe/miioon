b568bf21ed410a3568af146fafce1ea1b2a9a06c
Liu Yang <zation1@gmail.com>
2014-04-20 01:11:33 +0800

    refactor press

--- miioon/app/scripts/press/controller.js
+++ miioon/app/scripts/press/controller.js
@@ -3,20 +3,17 @@
 angular.module('2ViVe')
   .controller('PressController', ['$scope',
     function($scope) {
-      $scope.pressImagesBig;
       $scope.pressImagesIndex = 1;
       $scope.isPopupHide = 1;
 
       $scope.showPopup = function(target) {
         $scope.pressImagesIndex = target.getAttribute('data');
-        $scope.pressImagesBig = 'images/press/press-big-' + $scope.pressImagesIndex + '.jpg';
         $scope.isPopupHide = 0;
       };
 
       $scope.hidePopup = function() {
         $scope.isPopupHide = 1;
       };
-
     }
   ]);
 
