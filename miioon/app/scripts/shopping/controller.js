509f2caf4ce5be1c87ea74e35d14c3166a5c1b09
Liu Yang <zation1@gmail.com>
2014-04-20 01:02:02 +0800

    display price in shopping

--- miioon/app/scripts/shopping/controller.js
+++ miioon/app/scripts/shopping/controller.js
@@ -12,6 +12,13 @@ angular.module('2ViVe')
           Variants.remove(variant.id);
         });
       };
+      $scope.GrandTotal = function() {
+        var total = 0;
+        angular.forEach(Variants.data, function(variant) {
+          total += variant.price * Shopping.getByItemId(variant.id).quantity;
+        });
+        return total;
+      };
       $scope.$watch(function() {
         return Shopping.items;
       }, function() {
