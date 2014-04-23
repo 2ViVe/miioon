ccfff24abd9f992f813a4143acfa9c2b6ce06a77
kiddkai <kiddkai@gmail.com>
2014-04-24 07:23:25 +0800

    [view] zekai: add retail signup form - add register test and functional - call signup retail api

--- miioon/app/scripts/retailSignUp/controller.js
+++ miioon/app/scripts/retailSignUp/controller.js
@@ -10,7 +10,23 @@ angular.module('2ViVe')
       $scope.register = function() {
         $scope.retailSignupForm = $scope.retailSignupForm || {};
         if ($scope.retailSignupForm.$valid) {
-          Registration.createRetail();
+          Registration.createRetail(
+            $scope.sponsor,
+            $scope.login,
+            $scope.password,
+            $scope.email,
+            {
+              'first-name': $scope.firstName,
+              'last-name': $scope.lastName,
+              'stree': $scope.firstAddressLine,
+              'stree-cont': $scope.secondAddressLine,
+              'city': $scope.city,
+              'zip': $scope.zip,
+              'state-id': $scope.state.id,
+              'country-id': $scope.city.id,
+              'phone': $scope.phoneNumber
+            }
+          );
         }
       };
     }
--- miioon/test/spec/retailSignUp/controllerSpec.js
+++ miioon/test/spec/retailSignUp/controllerSpec.js
@@ -8,7 +8,6 @@ describe('RetailSignUpCtrl', function() {
 
   beforeEach(module('2ViVe'));
 
-
   beforeEach(inject(function($rootScope, $controller, Registration) {
     scope = $rootScope.$new();
     Reg = Registration;
@@ -19,6 +18,10 @@ describe('RetailSignUpCtrl', function() {
       $scope: scope,
       Registration: Registration
     });
+
+    basicInfo(scope);
+    shippingInfo(scope);
+
   }));
 
   afterEach(function() {
@@ -55,4 +58,65 @@ describe('RetailSignUpCtrl', function() {
   });
 
 
+  when('already fullfill the valid data', function() {
+    beforeEach(function() {
+      scope.retailSignupForm = scope.retailSignupForm || {};
+      scope.retailSignupForm.$valid = true;
+    });
+
+    beforeEach(function() {
+      scope.register();
+    });
+
+    it('should use the sponsor data to fill the form', function() {
+      expect(Reg.createRetail.args[0][0]).toBe(scope.sponsor);
+    });
+
+    it('should use the login data to fill the form', function() {
+      expect(Reg.createRetail.args[0][1]).toBe(scope.login);
+    });
+
+    it('should use the password data to fill the form', function() {
+      expect(Reg.createRetail.args[0][2]).toBe(scope.password);
+    });
+
+    it('should use the email data to fill the form', function() {
+      expect(Reg.createRetail.args[0][3]).toBe(scope.email);
+    });
+
+    it('should use the shipping address data to fill the form', function() {
+      expect(Reg.createRetail.args[0][4]).toEqual({
+        'first-name': scope.firstName,
+        'last-name': scope.lastName,
+        'stree': scope.firstAddressLine,
+        'stree-cont': scope.secondAddressLine,
+        'city': scope.city,
+        'zip': scope.zip,
+        'state-id': scope.state.id,
+        'country-id': scope.city.id,
+        'phone': scope.phoneNumber
+      });
+    });
+
+  });
+
+
+  function basicInfo(scope) {
+    scope.sponsor = '100001';
+    scope.login = 'kiddkai';
+    scope.email = 'kiddkai@gmail.com';
+    scope.password = 'zekaipass';
+  }
+
+  function shippingInfo(scope) {
+    scope.firstName = 'kidd';
+    scope.lastName = 'kai';
+    scope.phoneNumber = '123456';
+    scope.firstAddressLine = 'line one';
+    scope.secondAddressLine = 'line 2';
+    scope.city = 'Chicago';
+    scope.country = {id: 234};
+    scope.state = {id: 123};
+    scope.zip = '123';
+  }
 });
