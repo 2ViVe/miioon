1bcec1707e7cd1c6b24cda009e8781c84040d3c1
Liu Yang <zation1@gmail.com>
2014-04-27 02:34:26 +0800

    move route to route.js

---
+++ miioon/app/scripts/route.js
@@ -0,0 +1,73 @@
+'use strict';
+
+angular.module('miioonApp')
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html'
+      })
+      .when('/signin', {
+        templateUrl: 'views/sign-in.html',
+        controller: 'SignInController'
+      })
+      .when('/signup', {
+        templateUrl: 'views/sign-up/all.html',
+        controller: 'SignUpController'
+      })
+      .when('/retail-signup', {
+        templateUrl: 'views/sign-up/retail-signup.html',
+        controller: 'RetailSignUpController'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/taxon/:taxonId', {
+        templateUrl: 'views/taxon.html',
+        controller: 'TaxonController'
+      })
+      .when('/product/:productId', {
+        templateUrl: 'views/product-detail.html',
+        controller: 'ProductController'
+      })
+      .when('/taxon/:taxonId/sub-taxon/:subTaxonId', {
+        templateUrl: 'views/taxon.html',
+        controller: 'TaxonController'
+      })
+      .when('/host', {
+        templateUrl: 'views/host.html'
+      })
+      .when('/press', {
+        templateUrl: 'views/press.html',
+        controller: 'PressController'
+      })
+      .when('/brand', {
+        templateUrl: 'views/brand.html'
+      })
+      .when('/career', {
+        templateUrl: 'views/career.html'
+      })
+      .when('/gift/gift-card', {
+        templateUrl: 'views/gift/gift-card.html',
+        controller: 'GiftController'
+      })
+      .when('/gift/checkout', {
+        templateUrl: 'views/gift/gift-checkout.html'
+      })
+      .when('/shopping', {
+        templateUrl: 'views/shopping.html',
+        controller: 'ShoppingController'
+      })
+      .when('/t-c', {
+        templateUrl: 'views/t-c.html'
+      })
+      .when('/printing', {
+        templateUrl: 'views/printing.html'
+      })
+      .when('/checkout', {
+        templateUrl: 'views/checkout.html',
+        controller: 'CheckoutController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
