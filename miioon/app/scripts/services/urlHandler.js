06cc238079690c3889925445b69afefae1038e25
Liu Yang <zation1@gmail.com>
2014-04-28 03:21:18 +0800

    refactor magic number in urlHandler

--- miioon/app/scripts/services/urlHandler.js
+++ miioon/app/scripts/services/urlHandler.js
@@ -3,14 +3,19 @@
 angular.module('2ViVe')
   .factory('UrlHandler', ['$window', '$location',
     function($window, $location) {
+      var PORT_FOR_NON_SECURE_RETAIL_DEMO_SITE = 11442;
+      var PORT_FOR_SECURE_RETAIL_DEMO_SITE = 22442;
+      var PORT_FOR_BACK_OFFICE_DEMO_SITE = 33442;
+
       return {
         backOfficeUrl: function() {
           var port = $location.port();
           var protocol = $location.protocol();
           var url = $location.absUrl();
 
-          if (port === 11442 || port === 22442) {
-            url = url.replace(':' + port, ':33442');
+          if (port === PORT_FOR_NON_SECURE_RETAIL_DEMO_SITE ||
+            port === PORT_FOR_SECURE_RETAIL_DEMO_SITE) {
+            url = url.replace(':' + port, ':' + PORT_FOR_BACK_OFFICE_DEMO_SITE);
             return url.replace(protocol + ':', 'https:');
           }
 
@@ -27,14 +32,16 @@ angular.module('2ViVe')
 
           if (targetProtocol === 'http' && securePaths.indexOf(targetPath) >= 0) {
             targetUrl = targetUrl.replace('http://', 'https://');
-            if (targetPort === 11442) {
-              targetUrl = targetUrl.replace(':11442', ':22442');
+            if (targetPort === PORT_FOR_NON_SECURE_RETAIL_DEMO_SITE) {
+              targetUrl = targetUrl.replace(':' + PORT_FOR_NON_SECURE_RETAIL_DEMO_SITE,
+                  ':' + PORT_FOR_SECURE_RETAIL_DEMO_SITE);
             }
             $window.location.href = targetUrl;
           } else if (targetProtocol === 'https' && securePaths.indexOf(targetPath) < 0) {
             targetUrl = targetUrl.replace('https://', 'http://');
-            if (targetPort === 22442) {
-              targetUrl = targetUrl.replace(':22442', ':11442');
+            if (targetPort === PORT_FOR_SECURE_RETAIL_DEMO_SITE) {
+              targetUrl = targetUrl.replace(':' + PORT_FOR_SECURE_RETAIL_DEMO_SITE,
+                  ':' + PORT_FOR_NON_SECURE_RETAIL_DEMO_SITE);
             }
             $window.location.href = targetUrl;
           }
