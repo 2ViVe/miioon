3a5f9adeee93887b5938c46693c1945112e5923d
Liu Yang <zation1@gmail.com>
2014-04-28 02:49:10 +0800

    hanle secure locaiton redirection

--- miioon/app/scripts/app.js
+++ miioon/app/scripts/app.js
@@ -13,17 +13,39 @@ angular.module('miioonApp', [
   'mm.foundation.tabs',
   'mm.foundation.modal',
   'duScroll'
-]).run(['$rootScope', 'cfpLoadingBar', function($rootScope, cfpLoadingBar) {
-  $rootScope.$on('$routeChangeStart', function() {
-    cfpLoadingBar.start();
-  });
+]).run(['$rootScope', 'cfpLoadingBar', '$window', '$location',
+  function($rootScope, cfpLoadingBar, $window, $location) {
+    var securePaths = ['/signup', '/signin', '/checkout'];
 
-  $rootScope.$on('$routeChangeError', function() {
-    cfpLoadingBar.complete();
-  });
+    $rootScope.$on('$routeChangeStart', function() {
+      cfpLoadingBar.start();
 
-  $rootScope.$on('$viewContentLoaded', function() {
-    cfpLoadingBar.complete();
-  });
+      var targetPath = $location.path();
+      var targetProtocol = $location.protocol();
+      var targetUrl = $location.absUrl();
+      var targetPort = $location.port();
 
-}]);
+      if (targetProtocol === 'http' && securePaths.indexOf(targetPath) >= 0) {
+        targetUrl = targetUrl.replace('http://', 'https://');
+        if (targetPort === 11442) {
+          targetUrl = targetUrl.replace(':11442', ':22442');
+        }
+        $window.location.href = targetUrl;
+      } else if (targetProtocol === 'https' && securePaths.indexOf(targetPath) < 0) {
+        targetUrl = targetUrl.replace('https://', 'http://');
+        if (targetPort === 22442) {
+          targetUrl = targetUrl.replace(':22442', ':11442');
+        }
+        $window.location.href = targetUrl;
+      }
+    });
+
+    $rootScope.$on('$routeChangeError', function() {
+      cfpLoadingBar.complete();
+    });
+
+    $rootScope.$on('$viewContentLoaded', function() {
+      cfpLoadingBar.complete();
+    });
+
+  }]);
