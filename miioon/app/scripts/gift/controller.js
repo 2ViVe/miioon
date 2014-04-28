53b3cb6218fb1fc64a31df09a04cb60b04adbb54
after9 <ap66.ron@gmail.com>
2014-04-28 16:00:56 +0800

    gift card image update

--- miioon/app/scripts/gift/controller.js
+++ miioon/app/scripts/gift/controller.js
@@ -4,13 +4,6 @@ angular.module('2ViVe')
   .controller('GiftController', ['$scope',
     function($scope) {
 
-      // images switching
-      $scope.giftImagesBig = 'images/gift/gift-img-big-1.jpg';
-      $scope.giftImagesIndex = '1';
-      $scope.switchGiftImages = function(target) {
-        $scope.giftImagesIndex = target.getAttribute('data');
-        $scope.giftImagesBig = 'images/gift/gift-img-big-' + $scope.giftImagesIndex + '.jpg';
-      };
 
       //tabs
       $scope.tabs = [
