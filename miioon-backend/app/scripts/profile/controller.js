6dcc6484fb8fb954bb0064cea64e26b7302a4773
kiddkai <kiddkai@gmail.com>
2014-04-30 10:02:21 +0800

    zekai - add profile page - ajust stylesheet and add a lil bit logic to it

--- miioon-backend/app/scripts/profile/controller.js
+++ miioon-backend/app/scripts/profile/controller.js
@@ -1,4 +1,15 @@
 angular.module('miioonApp')
-  .controller('ProfileController', [function() {
+  .controller('ProfileController', ['$scope', function($scope) {
+    $scope.username = 'USERNAME';
+    $scope.email = 'EMAIL@EXAMPLE.com';
+
+    $scope.saveAccountInfo = function() {
+      $scope.editingAccount = !$scope.editingAccount;
+    };
+
+    $scope.savePassword = function() {
+      $scope.editingPassword = !$scope.editingPassword;
+    };
+
 
   }]);
