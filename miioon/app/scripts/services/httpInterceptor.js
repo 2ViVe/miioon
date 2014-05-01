b3c376aac15b505775644b39715d5132573f005c
Liu Yang <zation1@gmail.com>
2014-05-01 17:10:01 +0800

    remove client id and client secret in request

--- miioon/app/scripts/services/httpInterceptor.js
+++ miioon/app/scripts/services/httpInterceptor.js
@@ -1,12 +1,10 @@
 'use strict';
 
 angular.module('2ViVe')
-  .factory('HttpInterceptor', ['$q', 'LocalStorage', 'CLIENT_ID',
-    function($q, LocalStorage, CLIENT_ID) {
+  .factory('HttpInterceptor', ['$q', 'LocalStorage',
+    function($q, LocalStorage) {
       return {
         'request': function(config) {
-          config.headers['x-client-id'] = CLIENT_ID;
-          config.headers['x-client-secret'] = 'HeFsCAvsXTzpHWAqRVWCibsUYlF7gjpLRUAUw551r';
           var token = LocalStorage.getToken();
           if(token) {
             config.headers['X-Authentication-Token'] = token;
