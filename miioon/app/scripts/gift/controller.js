b4b98afd8987572bde621c87c063b184125d46d3
Liu Yang <zation1@gmail.com>
2014-05-03 00:40:33 +0800

    validate amount when purchase

--- miioon/app/scripts/gift/controller.js
+++ miioon/app/scripts/gift/controller.js
@@ -3,6 +3,8 @@
 angular.module('2ViVe')
   .controller('GiftController', ['$scope', '$modal', 'GiftCard',
     function($scope, $modal, GiftCard) {
+      $scope.submitted = false;
+
       var giftCard = new GiftCard();
       giftCard.fetch.success(function() {
         $scope.giftCards = giftCard.data.variants;
@@ -15,14 +17,30 @@ angular.module('2ViVe')
         });
       };
 
+      $scope.purchase = function() {
+        $scope.submitted = true;
+        var tabInValid = false;
+        angular.forEach($scope.tabs, function(tab) {
+          if (tab.active && this[tab.form].$invalid) {
+            tabInValid = true;
+            return null;
+          }
+        });
+        if (tabInValid || this.amountForm.$invalid) {
+          return null;
+        }
+      };
+
       $scope.tabs = [
         {
           title: 'Email',
-          url: 'views/gift/email.html'
+          url: 'views/gift/email.html',
+          form: 'emailForm'
         },
         {
           title: 'Post',
-          url: 'views/gift/post.html'
+          url: 'views/gift/post.html',
+          form: 'postForm'
         }
       ];
     }
