5c9a533a5680b74aed337e524437db6cfd74a7a2
Liu Yang <zation1@gmail.com>
2014-05-03 21:46:31 +0800

    valid email form in gift card

--- miioon/app/scripts/gift/controller.js
+++ miioon/app/scripts/gift/controller.js
@@ -1,10 +1,26 @@
 'use strict';
 
 angular.module('2ViVe')
+  .controller('GiftCardEmailFormController', ['$scope',
+    function($scope) {
+      $scope.$watch('emailForm.$invalid', function(invalid) {
+        $scope.formInvalid.email = invalid;
+      });
+    }])
+  .controller('GiftCardPostFormController', ['$scope',
+    function($scope) {
+      $scope.$watch('postForm.$invalid', function(invalid) {
+        $scope.formInvalid.post = invalid;
+      });
+    }])
   .controller('GiftController', ['$scope', '$modal', 'GiftCard',
     function($scope, $modal, GiftCard) {
       $scope.submitted = false;
       $scope.giftCardInfo = {};
+      $scope.formInvalid = {
+        'email': true,
+        'post': true
+      };
 
       var giftCard = new GiftCard();
       giftCard.fetch.success(function() {
@@ -21,8 +37,9 @@ angular.module('2ViVe')
       $scope.purchase = function() {
         $scope.submitted = true;
         var tabInValid = false;
+
         angular.forEach($scope.tabs, function(tab) {
-          if (tab.active && this[tab.form].$invalid) {
+          if (tab.active && $scope.formInvalid[tab.form].$invalid) {
             tabInValid = true;
             return null;
           }
@@ -36,17 +53,15 @@ angular.module('2ViVe')
         {
           title: 'Email',
           url: 'views/gift/email.html',
-          form: 'emailForm'
+          form: 'email'
         },
         {
           title: 'Post',
           url: 'views/gift/post.html',
-          form: 'postForm'
+          form: 'post'
         }
       ];
-    }
-  ]
-);
+    }]);
 
 
 
