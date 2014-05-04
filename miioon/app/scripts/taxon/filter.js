db550f8b31b1627186b50c18f592d4d5e212e73b
Liu Yang <zation1@gmail.com>
2014-05-04 15:23:11 +0800

    filter taxon is null

--- miioon/app/scripts/taxon/filter.js
+++ miioon/app/scripts/taxon/filter.js
@@ -3,7 +3,7 @@
 angular.module('2ViVe')
   .filter('taxonLink', function() {
     return function(taxon) {
-      if (taxon === undefined) {
+      if (taxon === null || taxon === undefined) {
         return;
       }
       var link = '/#/taxon/' + taxon.id;
