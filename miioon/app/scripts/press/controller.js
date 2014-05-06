525cfea899b92a9fc1fd8b5ec0d81cd67ed74b34
after9 <ap66.ron@gmail.com>
2014-05-06 16:28:14 +0800

    modal popup update for press page

--- miioon/app/scripts/press/controller.js
+++ miioon/app/scripts/press/controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
+/*
 angular.module('2ViVe')
   .controller('PressController', ['$scope',
-    function($scope) {
+  function($scope) {
       $scope.pressImagesIndex = 1;
       $scope.isPopupHide = 1;
 
@@ -17,5 +18,30 @@ angular.module('2ViVe')
     }
   ]);
 
+*/
+
+angular.module('2ViVe')
+  .controller('PressController', ['$scope', '$modal',
+    function($scope, $modal) {
+      $scope.pressImagesIndex = 1;
+      $scope.isPopupHide = 1;
+
+      $scope.showPopup = function(target) {
+        $scope.pressImagesIndex = target.getAttribute('data');
+        $modal.open({
+          templateUrl: 'views/press-popup.html',
+          controller: 'PressModalController',
+          scope: $scope
+        });
+      };
+    }
+  ])
+  .controller('PressModalController', ['$scope', '$modalInstance',
+    function($scope, $modalInstance) {
+      $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+      };
+    }
+  ]);
 
 
