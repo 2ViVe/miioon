051af534044efbb81631e64d661c024b3414c46f
Liu Yang <zation1@gmail.com>
2014-05-07 20:33:53 +0800

    redirect to retail site if retail user or not login

--- miioon-backend/app/scripts/initialize.js
+++ miioon-backend/app/scripts/initialize.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('miioonApp')
-  .run(['User', 'Profile', 'Shopping',
-    function(User, Profile, Shopping) {
+  .run(['User', 'Profile', '$window', 'UrlHandler',
+    function(User, Profile, $window, UrlHandler) {
       Profile.fetch();
-      User.fetch();
+      User.fetch().then(function() {
+        if (User.data.roleCode === 'R') {
+          $window.location.href = UrlHandler.retailUrl();
+        }
+      }, function() {
+        $window.location.href = UrlHandler.retailUrl();
+      });
     }]);
