6bcaa0c2bac656f78d86553895e982a6005fc426
kiddkai <kiddkai@gmail.com>
2014-05-07 09:54:39 +0800

    [http://demo.abovegem.com:3000/issues/541] pick day

---
+++ miioon-backend/app/scripts/inputs/datePicker.js
@@ -0,0 +1,18 @@
+angular.module('2ViVe')
+  .directive('datePicker', function() {
+    return {
+      restrict: 'A',
+      link: function (scope, element) {
+        var maxDate = moment().add(-18, 'years');
+        var minDate = moment().add(-150, 'years');
+
+        new Pikaday({
+          field: angular.element(element)[0],
+          yearRange: [minDate.year(), maxDate.year()],
+          defaultDate: moment().add(-30, 'years').toDate(),
+          minDate: minDate.toDate(),
+          maxDate: maxDate.toDate()
+        });
+      }
+    };
+  });
