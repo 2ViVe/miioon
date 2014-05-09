67b28fffaf0fb362c58e991684f61ae1cdbeead4
kiddkai <kiddkai@gmail.com>
2014-05-10 01:32:40 +0800

    [http://demo.abovegem.com:3000/issues/644] add giftcode functionality and service and controllers

--- miioon-backend/app/scripts/services/giftcard.js
+++ miioon-backend/app/scripts/services/giftcard.js
@@ -1,10 +1,12 @@
 angular.module('2ViVe')
-  .factory('GiftCard', ['$http', function($http) {
+  .factory('GiftCard', ['$http', 'CamelCaseLize', function($http, camelcase) {
 
     function GiftCard() {}
 
     GiftCard.fetch = function() {
-      return $http.get('/api/v2/giftcards').then(function(resp) {
+      return $http.get('/api/v2/giftcards', {
+        transformResponse:  camelcase
+      }).then(function(resp) {
         return resp.data.response;
       });
     };
