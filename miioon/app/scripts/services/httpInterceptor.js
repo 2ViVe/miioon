f58d3a6ec263f1c995a101cde80d4a852fd49b8e
Liu Yang <zation1@gmail.com>
2014-05-19 16:11:28 +0800

    refactor httpInterceptor

--- miioon/app/scripts/services/httpInterceptor.js
+++ miioon/app/scripts/services/httpInterceptor.js
@@ -4,10 +4,8 @@ angular.module('2ViVe')
   .factory('HttpInterceptor', ['$q', '$cookies', function($q, $cookies) {
     return {
       request: function(config) {
-        var deferred = $q.defer();
         delete $cookies.sid;
-        deferred.resolve(config);
-        return deferred.promise;
+        return config || $q.when(config);
       }
     };
   }]);
