1357784e43916c89f178792c3069ba1196d476aa
Liu Yang <zation1@gmail.com>
2014-05-26 17:55:33 +0800

    include 2ViVe module in karma

--- for-tails-only/karma.conf.js
+++ for-tails-only/karma.conf.js
@@ -17,6 +17,8 @@ module.exports = function(config) {
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/2ViVe/scripts/main.js',
+      'app/bower_components/2ViVe/scripts/services/*.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
