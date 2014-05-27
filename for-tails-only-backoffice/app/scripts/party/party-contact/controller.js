9ecab9e446af6d6d16df85cb3ac892423cea4d80
after9 <ap66.ron@gmail.com>
2014-05-27 15:33:46 +0800

    ftoApp change to fto

--- for-tails-only-backoffice/app/scripts/party/party-contact/controller.js
+++ for-tails-only-backoffice/app/scripts/party/party-contact/controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
-angular.module('ftoApp')
+angular.module('fto')
   //for party view invitation page
   .controller('PartyContactController', ['$scope', '$modal',
     function($scope, $modal) {
+      $scope.hahaha = 'hahahahahhaha';
       $scope.partyAddContact = function() {
         $modal.open({
           templateUrl: 'views/party/party-add-contact.html',
--- for-tails-only-backoffice/app/scripts/party/party-create/controller.js
+++ for-tails-only-backoffice/app/scripts/party/party-create/controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ftoApp')
+angular.module('fto')
   //for party view invitation page
   .controller('PartyCreateController', ['$scope', '$modal',
     function($scope, $modal) {
