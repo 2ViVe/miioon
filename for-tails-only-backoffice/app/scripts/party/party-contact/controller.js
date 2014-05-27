a935344d99f53a6cd1eb6e66918ee480c8f3d78c
after9 <ap66.ron@gmail.com>
2014-05-27 15:42:55 +0800

    wording update for FTO party planing

--- for-tails-only-backoffice/app/scripts/party/party-contact/controller.js
+++ for-tails-only-backoffice/app/scripts/party/party-contact/controller.js
@@ -4,7 +4,6 @@ angular.module('fto')
   //for party view invitation page
   .controller('PartyContactController', ['$scope', '$modal',
     function($scope, $modal) {
-      $scope.hahaha = 'hahahahahhaha';
       $scope.partyAddContact = function() {
         $modal.open({
           templateUrl: 'views/party/party-add-contact.html',
