c79bde0101cfc29ade62f12a6e2420dfbb46e849
xlaok <gaoyixin@joymap.cn>
2014-06-05 22:06:13 +0800

    bug fixes

--- backoffice/app/scripts/giftCode/controller.js
+++ backoffice/app/scripts/giftCode/controller.js
@@ -1,16 +1,14 @@
 'use strict';
 
 angular.module('2ViVe')
-  .controller('giftCodeCtrl', ['$scope', 'GiftCards', 'Email', function($scope, GiftCards, Email) {
+  .controller('giftCodeCtrl', ['$scope', 'GiftCards', function($scope, GiftCards) {
     GiftCards.fetch().then(function(result) {
       $scope.giftcodes = result;
     });
 
     $scope.sendEmail = function(giftcard){
     	var code = giftcard.code;
-    	Email.send(code).then(function(data){
-    		$scope.reciEmail = data.response['recipient-email'];
-    		$scope.success = data.response.success;
+    	GiftCards.resendEmail(code).then(function(data){
     		$scope.isSuccess = true;
     	});
     }
