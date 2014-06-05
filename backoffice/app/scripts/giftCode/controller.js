aa0fd10c3d121089b3293def74bde27a9bb13547
xlaok <gaoyixin@joymap.cn>
2014-06-05 19:11:06 +0800

    resend email

--- backoffice/app/scripts/giftCode/controller.js
+++ backoffice/app/scripts/giftCode/controller.js
@@ -1,8 +1,18 @@
 'use strict';
 
 angular.module('2ViVe')
-  .controller('giftCodeCtrl', ['$scope', 'GiftCards', function($scope, GiftCards) {
+  .controller('giftCodeCtrl', ['$scope', 'GiftCards', 'Email', function($scope, GiftCards, Email) {
     GiftCards.fetch().then(function(result) {
       $scope.giftcodes = result;
     });
+
+    $scope.sendEmail = function(giftcard){
+    	var code = giftcard.code;
+    	Email.send(code).then(function(data){
+    		$scope.reciEmail = data.response['recipient-email'];
+    		$scope.success = data.response.success;
+    		$scope.isSuccess = true;
+    	});
+    }
+
   }]);
