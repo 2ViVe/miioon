b09d040e07da38120245727edeb12e09742dcecc
kiddkai <kiddkai@gmail.com>
2014-06-26 23:29:29 +0800

    [zekai] - upgrade module - start replace the signup
    1. remove old signup js
    2. change all.html
    
    NEXT:
    
      - add  step 1

---
+++ retail/app/scripts/signUp/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular
+  .module('miioonApp/signup', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    '2ViVe',
+    'ui.utils'
+  ])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/signup', {
+        templateUrl: 'views/sign-up/all.html',
+        controller: 'SignUpController',
+        resolve: {
+          countries: ['Registration.Countries', function(Countries) {
+            return Countries.fetch();
+          }]
+        }
+      });
+//      .when('/retail-signup', {
+//        templateUrl: 'views/sign-up/retail.html',
+//        controller: 'RetailSignUpController'
+//      });
+  }]);
