448d939a3d1adddadddfe26b12caea326bc17027
Liu Yang <zation1@gmail.com>
2014-07-07 15:49:42 +0800

    fix product detail page

--- backoffice/app/scripts/product/controller.js
+++ backoffice/app/scripts/product/controller.js
@@ -1,24 +1,51 @@
 'use strict';
 
 angular.module('2ViVe')
-  .controller('ProductController', ['$scope', 'product', 'taxons', 'Shopping', 'LocalStorage', '$modal',
-    function($scope, product, taxons, Shopping, LocalStorage, $modal) {
-      var updateVariant = function() {
-        $scope.variant = product.getVariantByOptions({
-          'Color': $scope.selectedColor,
-          'Size': $scope.selectedSize
-        });
+  .controller('ProductController', ['$scope', 'product' , 'taxons', 'Shopping', '$modal',
+    function($scope, product, taxons, Shopping, $modal) {
+
+      $scope.personalizedTypes = product.data.personalizedTypes;
+      $scope.personalizedValues = [];
+
+      $scope.initPersonalizedValues = function(personalizedType, index) {
+        $scope.personalizedValues[index] = {
+          id: personalizedType.id,
+          name: personalizedType.name
+        };
       };
 
       $scope.product = product.data;
-      $scope.colors = product.colors;
-      $scope.sizes = product.sizes;
-      $scope.selectedColor = product.colors[0];
-      $scope.selectedSize = product.sizes[0];
-      $scope.currentImage = product.data.images[0];
-      $scope.catalogCode = product.catalogCode;
 
-      updateVariant();
+      $scope.allOptions = product.options;
+      $scope.currentOptions = {};
+      angular.forEach($scope.allOptions, function(options, optionType) {
+        $scope.currentOptions[optionType] = options[0];
+      });
+      $scope.variant = product.getVariantByOptions($scope.currentOptions);
+
+      $scope.changeOption = function(option, optionType) {
+        $scope.currentOptions[optionType] = option;
+        $scope.variant = product.getVariantByOptions($scope.currentOptions);
+
+        if ($scope.variant.images && $scope.variant.images.length > 0) {
+          $scope.currentImage = $scope.variant.images[0];
+          $scope.currentImages = $scope.variant.images;
+        } else {
+          $scope.currentImage = product.data.images[0];
+          $scope.currentImages = product.data.images;
+        }
+      };
+
+      $scope.getOptionStyle = function(option) {
+        if (option.presentationType === 'TXT' && option.presentationValue[0] === '#') {
+          return {
+            'background-color': option.presentationValue
+          };
+        }
+        return null;
+      };
+
+      $scope.catalogCode = product.catalogCode;
 
       $scope.subTaxon = taxons.getSubTaxonById(product.data.taxonId);
       if ($scope.subTaxon !== null) {
@@ -26,6 +53,9 @@ angular.module('2ViVe')
       } else {
         $scope.taxon = taxons.getById(product.data.taxonId);
       }
+
+      $scope.currentImage = product.data.images[0];
+      $scope.currentImages = product.data.images;
 
       $scope.changeImage = function(image) {
         $scope.currentImage = image;
@@ -35,18 +65,8 @@ angular.module('2ViVe')
         return image.replace('large', 'small');
       };
 
-      $scope.changeSize = function(size) {
-        $scope.selectedSize = size;
-        updateVariant();
-      };
-
-      $scope.changeColor = function(color) {
-        $scope.selectedColor = color;
-        updateVariant();
-      };
-
       $scope.addToCart = function() {
-        Shopping.add($scope.variant, $scope.quantity, product.catalogCode)
+        Shopping.add($scope.variant, $scope.quantity, product.catalogCode, $scope.personalizedValues)
           .success(function() {
             $modal.open({
               templateUrl: 'views/shopping/shopping-modal.html',
@@ -60,10 +80,4 @@ angular.module('2ViVe')
           });
       };
 
-      $scope.purchase = function() {
-        Shopping.add($scope.variant, $scope.quantity, product.catalogCode)
-          .success(function() {
-            Shopping.checkout();
-          });
-      };
     }]);
--- retail/app/scripts/product/controller.js
+++ retail/app/scripts/product/controller.js
@@ -3,23 +3,51 @@
 angular.module('2ViVe')
   .controller('ProductController', ['$scope', 'product', 'taxons', 'Shopping', 'LocalStorage', '$modal',
     function($scope, product, taxons, Shopping, LocalStorage, $modal) {
-      var updateVariant = function() {
-        $scope.variant = product.getVariantByOptions({
-          'Color': $scope.selectedColor,
-          'Size': $scope.selectedSize
-        });
-      };
-
       var replicateOwner = LocalStorage.getReplicateOwner();
       $scope.replicateOwnerLogin = replicateOwner ? replicateOwner.login : '';
 
+      $scope.personalizedTypes = product.data.personalizedTypes;
+      $scope.personalizedValues = [];
+
+      $scope.initPersonalizedValues = function(personalizedType, index) {
+        $scope.personalizedValues[index] = {
+          id: personalizedType.id,
+          name: personalizedType.name
+        };
+      };
+
       $scope.product = product.data;
-      $scope.colors = product.options.Color;
-      $scope.sizes = product.options.Size;
-      $scope.selectedColor = product.options.Color[0];
-      $scope.selectedSize = product.options.Size[0];
-      $scope.currentImage = product.data.images[0];
-      updateVariant();
+
+      $scope.allOptions = product.options;
+      $scope.currentOptions = {};
+      angular.forEach($scope.allOptions, function(options, optionType) {
+        $scope.currentOptions[optionType] = options[0];
+      });
+      $scope.variant = product.getVariantByOptions($scope.currentOptions);
+
+      $scope.changeOption = function(option, optionType) {
+        $scope.currentOptions[optionType] = option;
+        $scope.variant = product.getVariantByOptions($scope.currentOptions);
+
+        if ($scope.variant.images && $scope.variant.images.length > 0) {
+          $scope.currentImage = $scope.variant.images[0];
+          $scope.currentImages = $scope.variant.images;
+        } else {
+          $scope.currentImage = product.data.images[0];
+          $scope.currentImages = product.data.images;
+        }
+      };
+
+      $scope.getOptionStyle = function(option) {
+        if (option.presentationType === 'TXT' && option.presentationValue[0] === '#') {
+          return {
+            'background-color': option.presentationValue
+          };
+        }
+        return null;
+      };
+
+      $scope.catalogCode = product.catalogCode;
 
       $scope.subTaxon = taxons.getSubTaxonById(product.data.taxonId);
       if ($scope.subTaxon !== null) {
@@ -27,6 +55,9 @@ angular.module('2ViVe')
       } else {
         $scope.taxon = taxons.getById(product.data.taxonId);
       }
+
+      $scope.currentImage = product.data.images[0];
+      $scope.currentImages = product.data.images;
 
       $scope.changeImage = function(image) {
         $scope.currentImage = image;
@@ -36,18 +67,8 @@ angular.module('2ViVe')
         return image.replace('large', 'small');
       };
 
-      $scope.changeSize = function(size) {
-        $scope.selectedSize = size;
-        updateVariant();
-      };
-
-      $scope.changeColor = function(color) {
-        $scope.selectedColor = color;
-        updateVariant();
-      };
-
       $scope.addToCart = function() {
-        Shopping.add($scope.variant, $scope.quantity, product.catalogCode)
+        Shopping.add($scope.variant, $scope.quantity, product.catalogCode, $scope.personalizedValues)
           .success(function() {
             $modal.open({
               templateUrl: 'views/shopping/shopping-modal.html',
@@ -58,13 +79,6 @@ angular.module('2ViVe')
                 }]
               }
             });
-          });
-      };
-
-      $scope.purchase = function() {
-        Shopping.add($scope.variant, $scope.quantity, product.catalogCode)
-          .success(function() {
-            Shopping.checkout();
           });
       };
     }]);
